56894af9f3ceccc7af78928bc47cc5b28f8141f1
NYCSwan <megan.swanby@gmail.com>
2018-05-17 11:54:08 -0400

    Change nav styling to include mobile device format, add classes to clock li, change font color, nav width

--- src/navigation/navigation.react.jsx
+++ src/navigation/navigation.react.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Clock from '../clocks/clock.react';
 import Logo from '../media/cayor_color_2inch.jpg';
 import './navigation.css';
 
 const Navigation = props => (
 
-  <header className='header slideIn'>
-    <a
-      className='logo left'
-      href='/'
-      alt='homepage'>
+  <header className='header slide'>
+    <NavLink to='/' className='logo-container'>
       <img
+        className='logo'
         src={Logo}
         alt='logo' />
-    </a>
-    <ul className='nav'>
-      <li><a href='people'>PEOPLE</a></li>
-      <li><a href='approach' alt='Cayor Approach'>APPROACH</a></li>
-      <li><a href='opportunity' alt='Cayor Opportunities'>OPPORTUNITY</a></li>
-      <li><a href='esg' alt='ESG'>ESG</a></li>
-      <li onClick={props.handleClick}><Clock offset='2' /> ACCRA</li>
-      <li onClick={props.handleClick}><Clock offset='0'/> JOHANESSBURG</li>
+    </NavLink>
+    <ul className='nav right'>
+      <li><NavLink to='people' className='link'>PEOPLE</NavLink></li>
+      <li>
+        <NavLink to='approach' className='link'>APPROACH</NavLink></li>
+      <li>
+        <NavLink to='opportunity' className='link'>OPPORTUNITY</NavLink></li>
+      <li>
+        <NavLink to='esg' className='link'>ESG</NavLink></li>
+      <li
+        onClick={props.handleClick} className='clock'>
+        <Clock offset='2' />ACCRA</li>
+      <li
+      onClick={props.handleClick}
+      className='clock'>
+      <Clock offset='0'/>JOHANESSBURG</li>
     </ul>
   </header>
 )
