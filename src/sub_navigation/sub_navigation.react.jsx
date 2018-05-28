85044e52a810e77c541814edb4b8cde68f691050
NYCSwan <megan.swanby@gmail.com>
2018-05-28 17:46:09 -0400

    Add fontawesome, react-awesome, add icon to subnav and styling, fix nav styling, add new color variables

--- src/sub_navigation/sub_navigation.react.jsx
+++ src/sub_navigation/sub_navigation.react.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCaretRight from '@fortawesome/fontawesome-free-solid/faCaretRight';
 import './sub_navigation.css';
 
 const SubNav = (props) => (
   <aside className="sub_navigation">
-    <ul className={`list`}>
+    <ul className='list'>
       { props.navItems.map(navItem => {
         return (
-          <li key={navItem.value}
+          <li
+            key={navItem.value}
             onClick={props.handleClick}
             className={props.currentDetails === navItem.value ? 'active' : null}>
             {navItem.value}
+            <FontAwesomeIcon
+              icon={faCaretRight}
+              size='lg'
+              pull='right'
+              className='caret'
+            />
           </li>
         )
       }
