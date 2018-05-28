29fa3075939c66b5180d05a8c066b294b1e0883d
NYCSwan <megan.swanby@gmail.com>
2018-05-28 15:48:35 -0400

    Add active class and styling

--- src/sub_navigation/sub_navigation.react.jsx
+++ src/sub_navigation/sub_navigation.react.jsx
@@ -3,11 +3,12 @@ import './sub_navigation.css';
 
 const SubNav = (props) => (
   <aside className="sub_navigation">
-    <ul className='list'>
+    <ul className={`list`}>
       { props.navItems.map(navItem => {
         return (
           <li key={navItem.value}
-            onClick={props.handleClick}>
+            onClick={props.handleClick}
+            className={props.currentDetails === navItem.value ? 'active' : null}>
             {navItem.value}
           </li>
         )
