eff38cac783d8e4470ed8daa32542e7f2d114ad0
NYCSwan <megan.swanby@gmail.com>
2018-06-28 20:24:22 -0400

    Add handle close fn, style snapshot and pop-out box, etc

--- src/people/TeamDetails.react.jsx
+++ src/people/TeamDetails.react.jsx
@@ -8,51 +8,58 @@ import './team-details.css';
 class TeamDetails extends Component {
   state = {
     isOpen: false,
-    personDetails: []
+    personDetails: [],
+    currentPerson: ''
   }
 
   handleClick = (e) => {
     console.log('handle person click');
     const { teamDetails } = this.props;
-    const teamMember = []
-
+    const teamMember = [];
+    // debugger;
     if (e.target.className.includes('Fungai Ruwende')) {
-      // debugger;
       teamMember.push(teamDetails[0].summary);
       teamMember.push(teamDetails[0].experience);
       teamMember.push(teamDetails[0].education);
 
       this.setState({
-        personDetails: teamMember
+        personDetails: teamMember,
+        currentPerson: 'Fungai'
       })
     } else if ( e.target.className.includes('Dafe Diejomaoh')) {
       teamMember.push(teamDetails[1].summary);
       teamMember.push(teamDetails[1].experience);
       teamMember.push(teamDetails[1].education);
       this.setState({
-        personDetails: teamMember
+        personDetails: teamMember,
+        currentPerson: 'Dafe'
       })
     } else if ( e.target.className.includes('Yannick Mpollo')) {
       teamMember.push(teamDetails[2].summary);
       teamMember.push(teamDetails[2].experience);
       teamMember.push(teamDetails[2].education);
       this.setState({
-        personDetails: teamMember
+        personDetails: teamMember,
+        currentPerson: 'Yannick'
       })
     } else {
       teamMember.push(teamDetails[3].summary);
       teamMember.push(teamDetails[3].experience);
       teamMember.push(teamDetails[3].education);
       this.setState({
-        personDetails: teamMember
+        personDetails: teamMember,
+        currentPerson: 'Kofi'
       })
     }
     this.setState({ isOpen: true });
 
   }
 
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  }
   render() {
-    const { isOpen, personDetails } = this.state;
+    const { isOpen, personDetails, currentPerson } = this.state;
     const { teamDetails } = this.props;
 
     return (
@@ -61,9 +68,12 @@ class TeamDetails extends Component {
       { teamDetails.map(member => {
         return (
           <Snapshot
+            key={member.name}
             isOpen={isOpen}
+            currentPerson={currentPerson}
             name={member.name}
             role={member.position}
+            handleClose={this.handleClose}
             handleClick={this.handleClick}
             personDetails={personDetails} />
         )
--- src/people/snapshot.react.jsx
+++ src/people/snapshot.react.jsx
@@ -9,30 +9,41 @@ import './snapshot.css';
 
 const getImage = (name) => {
   if (name === 'Fungai Ruwende') {
-    return <img className={`headshot ${name}`} src={Fungai} alt='Photograph of Fungai Ruwende'/>;
+    return <img className={`headshot ${name}`} src={Fungai} alt='Photograph of Fungai Ruwende, Managing Partner Cayor Capital Sub-Saharan Africa Equity Fund' />;
   } else if (name === 'Dafe Diejomaoh') {
-    return <img className={`headshot ${name}`} src={Dafe} alt='Photograph of Dafe Diejomaoh'/>;
+    return <img className={`headshot ${name}`} src={Dafe} alt='Photograph of Dafe Diejomaoh, Managing Partner Cayor Capital Sub-Saharan Africa Equity Fund' />;
   } else if (name === 'Yannick Mpollo') {
-    return <img className={`headshot ${name}`} src={Yannick} alt='Photograph of Yannick Mpollo'/>;
+    return <img className={`headshot ${name}`} src={Yannick} alt='Photograph of Yannick Mpollo, Principal Cayor Capital Sub-Saharan Africa Equity Fund' />;
   } else {
-    return <img className={`headshot ${name}`} src={Kofi} alt='Photograph of Kofi Domfeh' />;
+    return <img className={`headshot ${name}`} src={Kofi} alt='Photograph of Kofi Domfeh, Vice President Cayor Capital Sub-Saharan Africa Equity Fund' />;
   }
 };
 
 const Snapshot = (props) => (
   <Fade
     in={props.fadeIn}
-    className={`snapshot ${props.name}`}
     key={props.name}
-    onClick={props.handleClick}>
-    {getImage(props.name)}
-    <div className='text'>
-      <h4 className={props.name}>{props.name}</h4>
-      <h5 className={props.name}>{props.role}</h5>
+
+    className={props.name.includes(props.currentPerson) ? `snapshot ${props.name} active` : 'snapshot'}>
+    <div
+      className='header'
+      onClick={props.handleClick}>
+      {getImage(props.name)}
+      { props.isOpen && props.name.includes(props.currentPerson) ?
+        null
+        :
+        <div className='text'>
+          <h4 className={props.name}>{props.name}</h4>
+          <h5 className={props.name}>{props.role}</h5>
+        </div>
+      }
     </div>
     <Collapse
-      isOpen={props.isOpen}>
+      isOpen={props.isOpen && props.name.includes(props.currentPerson)}>
       <PopOut
+        handleClose={props.handleClose}
+        name={props.name}
+        role={props.role}
         personDetails={props.personDetails} />
     </Collapse>
   </Fade>
