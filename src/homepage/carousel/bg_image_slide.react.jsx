84c6c8c629084b39cff6b2c6a537d4f2bc1a0cb4
NYCSwan <megan.swanby@gmail.com>
2018-09-26 12:05:10 -0400

    Move carousel items to homepage

--- src/homepage/carousel/bg_image_slide.react.jsx
+++ src/homepage/carousel/bg_image_slide.react.jsx
@@ -7,83 +7,24 @@ import {
   CarouselCaption,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
-// import { filter } from 'lodash';
-// import Button from '../../layout/button.react';
-import Slide1 from '../../media/slide1Cropped.jpeg';
-import Slide2 from '../../media/anastasiaCropped.jpeg';
-import Slide3 from '../../media/Accra.jpg';
-import Slide1Mobile from '../../media/slide1Mobile.jpeg';
-import Slide2Mobile from '../../media/slide2Mobile.jpeg';
-import Slide3Mobile from '../../media/slide3Mobile.jpeg';
-import Slide1Tablet from '../../media/slide1Mobile.jpeg';
-import Slide2Tablet from '../../media/slide2Mobile.jpeg';
-import Slide3Tablet from '../../media/slide3Mobile.jpeg';
 import './bg_image_slide.css';
-
-const items = [
-  {
-    src: [Slide1, Slide1Mobile, Slide1Tablet],
-    name: 'Slide1',
-    altText:
-      'We are a principal investment firm focused on investing in high growth sectors in select sub-Saharan African countries.',
-    caption:
-      'We are a principal investment firm focused on investing in high growth sectors in select sub-Saharan African countries.',
-    header: 'Invested In Africa.',
-    link: '/approach',
-  },
-  {
-    src: [Slide2, Slide2Mobile, Slide2Tablet],
-    name: 'Slide2',
-    altText:
-      'We are driven and entrepreneurial, and leverage the team’s combined 30-year track record investing in Africa to generate superior risk-adjusted returns for our investor partners.',
-    caption:
-      'We are driven and entrepreneurial and leverage the team’s combined 30-year track record investing in Africa to generate superior risk-adjusted returns for our investor partners.',
-    header: "Building New Platforms In Africa's Middle Markets.",
-    link: '/opportunity',
-  },
-  {
-    src: [Slide3, Slide3Mobile, Slide3Tablet],
-    name: 'Slide3',
-    altText:
-      'The product of our experience over time is a consistent, disciplined and value added methodology that has yielded attractive investment returns.',
-    caption:
-      'The product of our experience over time is a consistent, disciplined and value added methodology that has yielded attractive investment returns.',
-    header: 'Deep African Expertise. Consistent Sustainable Value.',
-    link: '/esg',
-  },
-];
 
 class BgImageSlide extends Component {
   state = {
     activeIndex: 0,
     direction: 'prev',
-    // deviceIdx: 2,
   };
 
-  // componentDidMount() {
-  //   this.updateDimensions();
-  // }
-
   shouldComponentUpdate(nextState, nextProps) {
-    return this.props.width !== nextProps.width;
+    return (
+      this.props.width !== nextProps.width ||
+      this.props.deviceIdx !== nextProps.deviceIdx
+    );
   }
 
   componentWillUnmount() {
     this.animating = false;
   }
-
-  // updateDimensions = () => {
-  //   console.log('update dimensions');
-  //   // debugger;
-  //   if (this.props.width <= 490 && this.props.width > 0) {
-  //     this.setState({ deviceIdx: 1 });
-  //   } else if (this.props.width >= 780 && this.props.width < 1025) {
-  //     this.setState({ deviceIdx: 2 });
-  //   } else {
-  //     this.setState({ deviceIdx: 0 });
-  //     console.log('deviceIdx 0', this.props.width);
-  //   }
-  // };
 
   onExiting = () => {
     console.log('on exiting');
@@ -99,7 +40,7 @@ class BgImageSlide extends Component {
     console.log('next');
     if (this.animating) return;
     const nextIndex =
-      this.state.activeIndex === items.length - 1
+      this.state.activeIndex === this.props.items.length - 1
         ? 0
         : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex, direction: 'next' });
@@ -110,7 +51,7 @@ class BgImageSlide extends Component {
     if (this.animating) return;
     const nextIndex =
       this.state.activeIndex === 0
-        ? items.length - 1
+        ? this.props.items.length - 1
         : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex, direction: 'prev' });
   };
@@ -124,7 +65,7 @@ class BgImageSlide extends Component {
   render() {
     console.log('render bg image');
     const { activeIndex } = this.state;
-    const { deviceIdx } = this.props;
+    const { deviceIdx, items } = this.props;
     // debugger;
     const slides = items.map(item => {
       return (
@@ -148,10 +89,10 @@ class BgImageSlide extends Component {
         </CarouselItem>
       );
     });
-    // ride='carousel'
     return (
       <Carousel
         interval={10000}
+        ride="carousel"
         activeIndex={activeIndex}
         next={this.next}
         previous={this.previous}
--- src/homepage/homepage.react.jsx
+++ src/homepage/homepage.react.jsx
@@ -2,26 +2,74 @@ import React from 'react';
 import BgImageSlide from './carousel/bg_image_slide.react';
 import './homepage.css';
 import Navigation from '../navigation/navigation.react';
+import Slide1 from '../media/slide1Cropped.jpeg';
+import Slide2 from '../media/anastasiaCropped.jpeg';
+import Slide3 from '../media/Accra.jpg';
+import Slide1Mobile from '../media/slide1Mobile.jpeg';
+import Slide2Mobile from '../media/slide2Mobile.jpeg';
+import Slide3Mobile from '../media/slide3Mobile.jpeg';
+import Slide1Tablet from '../media/slide1Mobile.jpeg';
+import Slide2Tablet from '../media/slide2Mobile.jpeg';
+import Slide3Tablet from '../media/slide3Mobile.jpeg';
+
+const items = [
+  {
+    src: [Slide1, Slide1Mobile, Slide1Tablet],
+    name: 'Slide1',
+    altText:
+      'We are a principal investment firm focused on investing in high growth sectors in select sub-Saharan African countries.',
+    caption:
+      'We are a principal investment firm focused on investing in high growth sectors in select sub-Saharan African countries.',
+    header: 'Invested In Africa.',
+    link: '/approach',
+  },
+  {
+    src: [Slide2, Slide2Mobile, Slide2Tablet],
+    name: 'Slide2',
+    altText:
+      'We are driven and entrepreneurial, and leverage the team’s combined 30-year track record investing in Africa to generate superior risk-adjusted returns for our investor partners.',
+    caption:
+      'We are driven and entrepreneurial and leverage the team’s combined 30-year track record investing in Africa to generate superior risk-adjusted returns for our investor partners.',
+    header: "Building New Platforms In Africa's Middle Markets.",
+    link: '/opportunity',
+  },
+  {
+    src: [Slide3, Slide3Mobile, Slide3Tablet],
+    name: 'Slide3',
+    altText:
+      'The product of our experience over time is a consistent, disciplined and value added methodology that has yielded attractive investment returns.',
+    caption:
+      'The product of our experience over time is a consistent, disciplined and value added methodology that has yielded attractive investment returns.',
+    header: 'Deep African Expertise. Consistent Sustainable Value.',
+    link: '/esg',
+  },
+];
 
 class Homepage extends React.Component {
   state = {
-    deviceIdx: 2,
+    deviceIdx: null,
   };
 
-  componentDidMount() {
-    this.updateDimensions();
+  componentDidUpdate(prevProps) {
+    if (this.props.width !== prevProps.width) {
+      this.updateDimensions();
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return this.props.width !== nextProps.width;
+    return (
+      this.props.width !== nextProps.width ||
+      this.state.deviceIdx !== nextState.deviceIdx
+    );
   }
 
   updateDimensions = () => {
     console.log('update dimensions');
-    // debugger;
-    if (this.props.width <= 490 && this.props.width > 0) {
+    if (this.props.width === null) {
+      return;
+    } else if (this.props.width <= 490 && this.props.width > 0) {
       this.setState({ deviceIdx: 1 });
-    } else if (this.props.width >= 780 && this.props.width < 1025) {
+    } else if (this.props.width >= 780 && this.props.width <= 1024) {
       this.setState({ deviceIdx: 2 });
     } else {
       this.setState({ deviceIdx: 0 });
@@ -31,6 +79,8 @@ class Homepage extends React.Component {
 
   render() {
     console.log('render homepage');
+    const { deviceIdx } = this.state;
+
     return (
       <div className="homepage">
         <Navigation
@@ -42,9 +92,10 @@ class Homepage extends React.Component {
           handleClose={this.props.handleClose}
           isContactModalOpen={this.props.isContactModalOpen}
         />
-        {this.props.width !== null && (
-          <BgImageSlide deviceIdx={this.state.deviceIdx} />
-        )}
+        {this.props.width !== null &&
+          deviceIdx !== null && (
+            <BgImageSlide deviceIdx={deviceIdx} items={items} />
+          )}
       </div>
     );
   }
