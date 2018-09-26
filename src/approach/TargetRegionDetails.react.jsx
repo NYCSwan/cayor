1cb49ff76b9b5dbb721eda907dd940780fac18ca
NYCSwan <megan.swanby@gmail.com>
2018-09-25 22:17:12 -0400

    Remove dead code. Lodash

--- src/approach/TargetRegionDetails.react.jsx
+++ src/approach/TargetRegionDetails.react.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import TextTable from '../layout/text-table.react';
 import './region-details.css';
 
-const RegionDetails = (props) => (
+const RegionDetails = props => (
   <div className="regionDetails">
-  {props.text.map(region => {
-    return (
-      <div className={region.className}>
-        <TextTable
-          location={props.location}
-          key={region}
-          text={region}
-        />
-      </div>
-    )
-  })}
+    {props.text.map(region => {
+      return (
+        <div className={region.className}>
+          <h4>We focus on multiple regions across Sub-Saharan Africa</h4>
+          <TextTable location={props.location} key={region} text={region} />
+        </div>
+      );
+    })}
   </div>
-)
+);
 
 export default RegionDetails;
--- src/layout/pop-out.react.jsx
+++ src/layout/pop-out.react.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { Card, CardBody, CardText, CardSubtitle } from 'reactstrap';
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-// import { faWindowClose } from '@fortawesome/fontawesome-free-solid';
 import './pop-out.css';
 
-// <FontAwesomeIcon
-//   onClick={props.handleClose}
-//   className={'close'}
-//   icon={faWindowClose}
-//   pull='right' />
 const PopOut = props => {
   const bodyHeight = Math.floor(props.height / 2.5);
 
