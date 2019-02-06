fa7b8f50be2e337400ee494b1d74be75153e40b4
NYCSwan <megan.swanby@gmail.com>
2019-02-06 12:16:00 -0500

    update people imgs

--- src/people/people.react.jsx
+++ src/people/people.react.jsx
@@ -4,9 +4,9 @@ import SubNav from "../sub_navigation/sub_navigation.react";
 import TeamDetails from "./TeamDetails.react";
 import TextTableContainer from "../layout/text-table-container.react";
 import Dafe from "../media/Dafe.jpg";
-import Fungai from "../media/Dafe.jpg";
-import Yannick from "../media/Dafe.jpg";
-import Kofi from "../media/Dafe.jpg";
+import Fungai from "../media/fungai.jpg";
+import Yannick from "../media/yannick.jpg";
+import Kofi from "../media/KDomfeh.Portraitfull.jpg";
 
 import "./people.css";
 
